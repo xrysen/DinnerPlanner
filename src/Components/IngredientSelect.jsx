563aee1e854d4ac7476daad945ae3d14c40e6ba8
Sean Oyler <xrysen@gmail.com>
2021-05-05 13:07:40 -0700

    Changes select to an autocomplete textfield

--- src/Components/IngredientSelect.jsx
+++ src/Components/IngredientSelect.jsx
@@ -2,9 +2,12 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
 import { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import AutoComplete from "@material-ui/lab/Autocomplete";
 
 const IngredientSelect = (props) => {
   const [ingredients, setIngredients] = useState([]);
+  const [chosen, setChosen] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8080/ingredients")
@@ -12,17 +15,19 @@ const IngredientSelect = (props) => {
       .then((res) => setIngredients(res));
   }, [ingredients]);
 
+  const handleChange = (event) => {
+    setChosen(event.target.value);
+  }
+
   return (
-    <div>
-      <InputLabel id={props.label}>{props.label}</InputLabel>
-      <Select labelId={props.label} onChange={props.setIngredient}>
-        <MenuItem>Add New Ingredient</MenuItem>
-        {ingredients.map((item)=> {
-          return (
-            <MenuItem key = {item.id} value={item.name}>{item.name}</MenuItem>
-          )
-        })}
-      </Select>
+    <div style={{ width: 200 }}>
+      <AutoComplete
+      id="ingredients"
+      options = {ingredients}
+      getOptionLabel={(option) => option.name}
+      renderInput={(params) => <TextField {...params} label="Ingredients" varient="outlined" />}
+      onChange = {handleChange}
+      />
     </div>
   );
 };
--- src/Components/NewIngredient.jsx
+++ src/Components/NewIngredient.jsx
@@ -5,37 +5,45 @@ import Icon from "@material-ui/core/Icon";
 import "./NewIngredient.css";
 
 const NewIngredient = () => {
-  const [ingredients, setIngredients] = useState([<IngredientSelect key = {0} />]);
+  const [ingredients, setIngredients] = useState([
+    <IngredientSelect key={0} />,
+  ]);
 
   const increaseIngredient = () => {
     setIngredients([
       ...ingredients,
-      <IngredientSelect key={ingredients.length}/>
-    ])
-  }
+      <IngredientSelect key={ingredients.length} />,
+    ]);
+  };
 
   const decreaseIngredient = () => {
     let arr = [...ingredients];
     arr.pop();
     if (arr.length === 0) {
-      arr = [<IngredientSelect key = {0} />]
+      arr = [<IngredientSelect key={0} />];
     }
     setIngredients(arr);
-  }
+  };
 
   return (
     <div>
       <h1>Add New Recipe</h1>
       <form>
-        <TextField id = "standard-basic" label = "Recipe Name" />
-        {ingredients.map((item) => {
-          return item
-        })}
-        <Icon className="add-button" onClick={()=> increaseIngredient()}>add_circle</Icon>
-        <Icon className="add-button" onClick={()=> decreaseIngredient()}>remove</Icon>
+        <TextField id="standard-basic" label="Recipe Name" />
+        <div className="ingredient-list">
+          {ingredients.map((item) => {
+            return item;
+          })}
+        </div>
+        <Icon className="add-button" onClick={() => increaseIngredient()}>
+          add_circle
+        </Icon>
+        <Icon className="add-button" onClick={() => decreaseIngredient()}>
+          remove
+        </Icon>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default NewIngredient;
