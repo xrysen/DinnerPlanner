3b187d3638fd280ad0768776ebe57f101c167256
Sean Oyler <xrysen@gmail.com>
2021-05-07 12:53:56 -0700

    Updates seed data

--- db/seeds/recipe.sql
+++ db/seeds/recipe.sql
@@ -1,2 +1,29 @@
 INSERT INTO recipe (name) VALUES ('Quinoa Fried Rice with Sticky Spiced Chicken');
+
+INSERT INTO direction (directions, recipe_id) VALUES ('Season chicken with 5 spice and salt', 1);
+INSERT INTO direction (directions, recipe_id) VALUES ('Cook in oil 5-7 minutes each side', 1);
+INSERT INTO direction (directions, recipe_id) VALUES ('Brush sweet chili sauce and sesame seeds onto chicken when cooked', 1);
 INSERT INTO direction (directions, recipe_id) VALUES ('Cook Quinoa', 1);
+INSERT INTO direction (directions, recipe_id) VALUES ('Using the same pan as the chicken add onions, pepper, brocolli and carrots', 1);
+INSERT INTO direction (directions, recipe_id) VALUES ('Add 1/2 tsp salt and some pepper. Cook for 7 minutes stirring a few times', 1);
+INSERT INTO direction (directions, recipe_id) VALUES ('Add garlic, ginger and cook for 2 minutes', 1);
+INSERT INTO direction (directions, recipe_id) VALUES ('Lightly beat eggs and add to pan stirring until a very soft scramble', 1);
+INSERT INTO direction (directions, recipe_id) VALUES ('Add peas soy sauce, seasame oil, green onion and salt', 1);
+
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 18, '1 1/4 Cups');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 19, '1/2');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 20, '1');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 21, '2 Cups');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 22, '2');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 23, '4 Cloves');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 24, '2 tsp');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 25, '1 Cup');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 26, '1.5 tbs');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 27, '1 tsp');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 28, '1/4 Cup');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 29, '1 tbs');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 30, '1 tsp');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 31, '1 tbs');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 32, '10');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 33, '2-3 tbs');
+INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement) VALUES(1, 34, '2-3 tbs');
