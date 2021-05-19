a0adde8ef40f33814cf2e920289e99384b2eabdd
Sean Oyler <xrysen@gmail.com>
2021-05-19 00:16:31 -0700

    Minor code cleanup

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -8,27 +8,28 @@ import Paper from "@material-ui/core/Paper";
 import Switch from "@material-ui/core/Switch";
 import "./Calendar.scss";
 import { useState, useEffect } from "react";
-import e from "cors";
+import Link from "@material-ui/core/Link";
+import Modal from "@material-ui/core/Modal";
 
 const Calendar = () => {
   const [dinners, setDinners] = useState([]);
   const [recipes, setRecipes] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [checked, setChecked] = useState(false);
-  const [used, setUsed] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:8080/recipes")
-    .then((res) => res.json())
-    .then((res) => {
-      setRecipes(res)
-      setLoaded(true);
-    });
+      .then((res) => res.json())
+      .then((res) => {
+        setRecipes(res);
+        setLoaded(true);
+      });
   }, []);
 
   const handleCheck = (e) => {
-    setChecked(e.target.checked)
-  }
+    setChecked(e.target.checked);
+  };
 
   const generateRecipes = () => {
     setDinners([]);
@@ -38,53 +39,56 @@ const Calendar = () => {
       recipeToUse = Math.floor(Math.random() * recipes.length) + 1;
       if (arr.indexOf(recipeToUse) === -1) {
         arr.push(recipeToUse);
-        if (recipes[recipeToUse - 1].has_leftovers && checked && arr.length < 5) {
+        if (
+          recipes[recipeToUse - 1].has_leftovers &&
+          checked &&
+          arr.length < 5
+        ) {
           arr.push(recipeToUse);
         }
       }
     }
-    console.log(recipes);
-    console.log(arr);
     setDinners(arr);
-  }
+  };
 
   return (
-    <div className = "calendar-container">
+    <div className="calendar-container">
+      <TableContainer component={Paper}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Sunday</TableCell>
+            <TableCell>Monday</TableCell>
+            <TableCell>Tuesday</TableCell>
+            <TableCell>Wednesday</TableCell>
+            <TableCell>Thursday</TableCell>
+            <TableCell>Friday</TableCell>
+            <TableCell>Saturday</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            {dinners.length
+              ? dinners.map((item) => {
+                  return (
+                    <TableCell>
+                      <Link className="recipe-link">
+                        {recipes[item - 1].name}
+                      </Link>
+                    </TableCell>
+                  );
+                })
+              : ""}
+          </TableRow>
+        </TableBody>
+      </TableContainer>
+      Use Leftovers? <Switch checked={checked} onChange={handleCheck} />
+      <br />
+      <button className="pink-button" onClick={() => generateRecipes()}>
+        Generate
+      </button>
 
-    <TableContainer component = {Paper}>
-      <TableHead>
-        <TableRow>
-          <TableCell>Sunday</TableCell>
-          <TableCell>Monday</TableCell>
-          <TableCell>Tuesday</TableCell>
-          <TableCell>Wednesday</TableCell>
-          <TableCell>Thursday</TableCell>
-          <TableCell>Friday</TableCell>
-          <TableCell>Saturday</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        <TableRow>
-          {dinners.length ? dinners.map((item) => {
-           return <TableCell>{recipes[item - 1].name}</TableCell>
-          }) : ""}
-          {/* <TableCell>Burritos</TableCell>
-          <TableCell>Burritos</TableCell>
-          <TableCell>Pizza</TableCell>
-          <TableCell>Pizza</TableCell>
-          <TableCell>Pizza</TableCell>
-          <TableCell>Pizza</TableCell>
-          <TableCell>Pizza</TableCell> */}
-          
-        </TableRow>
-      </TableBody>
-    </TableContainer>
-    Use Leftovers? <Switch checked = {checked} onChange={handleCheck} />
-    <br />
-    <button className = "pink-button" onClick = {()=> generateRecipes()}>Generate</button>
     </div>
-  )
-
-}
+  );
+};
 
 export default Calendar;
