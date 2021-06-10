1ad9615b915099f19832cf5ebb4d513385c0a8f3
Sean Oyler <xrysen@gmail.com>
2021-06-10 13:11:05 -0700

    One more time

--- server/server.js
+++ server/server.js
@@ -107,16 +107,17 @@ app.get("/food_types", (req, res) => {
   db.getFoodTypes().then((result) => res.status(200).send(result.rows));
 });
 
-app.listen(PORT, () => {
-  console.log("Server started listening on port " + PORT);
-});
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
   app.use(express.static("build"));
-
+  
   // Express will serve up the front-end index.html file if it doesn't recognize the route
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve("build", "index.html"))
+  res.sendFile(path.resolve("build", "index.html"))
   );
 }
+
+app.listen(PORT, () => {
+  console.log("Server started listening on port " + PORT);
+});
