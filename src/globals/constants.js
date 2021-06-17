6872b59896296c0a282625af18a0dcc7908eb7ee
Sean Oyler <xrysen@gmail.com>
2021-06-17 12:51:09 -0700

    Updates ENDPOINT for deployment

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,5 +1,5 @@
-// const ENDPOINT = "https://calm-gorge-82183.herokuapp.com";
-const ENDPOINT = "http://localhost:8080";
+const ENDPOINT = "https://calm-gorge-82183.herokuapp.com";
+// const ENDPOINT = "http://localhost:8080";
 
 
 exports.ENDPOINT = ENDPOINT;
